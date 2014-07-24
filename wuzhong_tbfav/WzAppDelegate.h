04d2b7eea1fbc2df990899f1991ab844d91f8c89
武仲 <wuzhong@taobao.com>
2014-07-24 20:03:14 +0800

    Initial Commit

---
+++ wuzhong_tbfav/WzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WzAppDelegate.h
+//  wuzhong_tbfav
+//
+//  Created by wuzhong on 14-7-24.
+//  Copyright (c) 2014年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ wuzhong_tbfav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  wuzhong_tbfav
+//
+//  Created by wuzhong on 14-7-24.
+//  Copyright (c) 2014年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WzAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WzAppDelegate class]));
+    }
+}
